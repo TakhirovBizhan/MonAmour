7078422e78492efcf5cba994fbc29bb9af460312
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-03-31 02:14:48 +0300

    fix: fixed docker files

--- backend/wait_for_postgres.sh
+++ backend/wait_for_postgres.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# Устанавливаем пароль для psql
-export PGPASSWORD="$DB_PASSWORD"
+echo "POSTGRES_HOST: $POSTGRES_HOST"
+echo "POSTGRES_USER: $POSTGRES_USER"
+echo "POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
+echo "POSTGRES_DB: $POSTGRES_DB"
+
+# Устанавливаем пароль для psql из переменной окружения
+export PGPASSWORD="$POSTGRES_PASSWORD"
 
 # Проверка подключения к базе данных PostgreSQL
-until psql -h "db" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
+until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   echo "Waiting for PostgreSQL to be available..."
   sleep 2
 done
 
-echo "PostgreSQL is up and running!
+echo "PostgreSQL is up and running!"
