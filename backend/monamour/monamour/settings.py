7faf636e03bfd741d09ca95bfc066fabdf1fefb5
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-05-21 11:16:08 +0300

    chore: some prep for frontend

--- backend/monamour/monamour/settings.py
+++ backend/monamour/monamour/settings.py
@@ -48,6 +48,13 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    
+    'DEFAULT_PERMISSION_CLASSES': [
+        # По умолчанию должен быть AllowAny:
+        'rest_framework.permissions.AllowAny',
+    ],
+    
+    
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
